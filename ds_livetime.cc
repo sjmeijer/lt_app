c1df665bcbe10559b67d51a7d08050f0569178f3
Sam <samuelmeijer@gmail.com>
2017-09-24 16:26:57 -0400

    Trying with the right formula now

--- ds_livetime.cc
+++ ds_livetime.cc
@@ -694,8 +694,8 @@ void calculateLiveTime(vector<int> runList, int dsNum, bool raw, bool runDB, boo
       double totalLTUnc = sqrt(channelRuntimeStd2[chan] + ltHWUnc*ltHWUnc)/(3600*24);
       // double totalLTUnc = sqrt(channelRuntimeStd2[chan])/(3600*24);   // no contribution from deadtime uncertainty
       
-      bestExposureUnc[detChanToDetIDMap[chan]]   = channelExposure[chan]*( (activeMassUnc/activeMass)*(activeMassUnc/activeMass)  + (totalLTUnc/livetime)*(totalLTUnc/livetime) ); 
-      bestExposureLTUnc[detChanToDetIDMap[chan]] = channelExposure[chan]*(                           0                            + (totalLTUnc/livetime)*(totalLTUnc/livetime) ); 
+      bestExposureUnc[detChanToDetIDMap[chan]]   = channelExposure[chan]*channelExposure[chan]*( (activeMassUnc/activeMass)*(activeMassUnc/activeMass)  + (totalLTUnc/livetime)*(totalLTUnc/livetime) ); 
+      bestExposureLTUnc[detChanToDetIDMap[chan]] = channelExposure[chan]*channelExposure[chan]*(                           0                            + (totalLTUnc/livetime)*(totalLTUnc/livetime) ); 
       }
 
     // only add to the final values ONCE for each detector!
